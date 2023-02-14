b0f9c38024b8834a1fbf9ebbc194e049f8a01afb
usarboyev <ausarboyev@gmail.com>
2023-02-15 04:11:59 +0500

    updated
    yangiliklar sayti

--- news_app/urls.py
+++ news_app/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from .views import (
     news_list, news_detail, ContactPageView, HomePageView,
     LocalNewsPageView, GlobalNewsPageView, SportNewsPageView,
-    TechNewsPageView
+    TechNewsPageView, NewsDeleteView, NewsUpdateView, NewsCreateView
 )
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home_page'),
     path('news/all/', news_list, name='news_list'),
+    path('news/create/', NewsCreateView.as_view(), name='news_create'),
     path('news/<slug:news>/', news_detail, name='news_detail_page'),
+    path('news/<slug>/edit/', NewsUpdateView.as_view(), name='news_update'),
+    path('news/<slug>/delete/', NewsDeleteView.as_view(), name='news_delete'),
     path('contact/', ContactPageView.as_view(), name='contact_page'),
     path('local/', LocalNewsPageView.as_view(), name='local_news_page'),
     path('global/', GlobalNewsPageView.as_view(), name='global_news_page'),
--- news_app/views.py
+++ news_app/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+
 from .models import News, Category
 from .forms import ContactForm
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, UpdateView, DeleteView, CreateView
 
 
 def news_list(request):
@@ -110,3 +112,22 @@ class SportNewsPageView(ListView):
     def get_queryset(self):
         news = self.model.published.all().filter(category__name='Sport')
         return news
+
+class NewsUpdateView(UpdateView):
+    model = News
+    fields = ('title', 'body', 'image', 'status','category')
+    template_name = 'crud/news_edit.html'
+
+
+class NewsDeleteView(DeleteView):
+    model = News
+    template_name = 'crud/news_delete.html'
+    success_url = reverse_lazy('home_page')
+
+
+class NewsCreateView(CreateView):
+    model = News
+    template_name = 'crud/news_create.html'
+    fields = ('title','slug','body','image','category','status')
+    prepopulated_fields = {'slug': ('title',)}
+    # success_url = reverse_lazy('home_page')
